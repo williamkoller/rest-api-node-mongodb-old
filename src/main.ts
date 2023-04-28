a0303746ba6070302c4a29f55cd00d6e51a30b52
William Koller <williamkoller30@gmail.com>
2023-04-28 14:33:38 -0300

    feat: add new ValidationPipe in main

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const config = app.get(ConfigService);
   const port = config.get<number>('PORT');
   await app.listen(port, () =>
